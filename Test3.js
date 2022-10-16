b5b33b071def906169cc00bf9447cebb93ac796e
Abdallah-92 <115651669+Abdallah-92@users.noreply.github.com>
2022-10-16 18:48:45 +0800

    Add files via upload

---
+++ Test3.js
@@ -0,0 +1,4 @@
+var vg_2 = "line_graph.json";
+vegaEmbed("#graph_vis", vg_2).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
---
+++ Test4.js
@@ -0,0 +1,4 @@
+var vg_2 = "bar_chart.json";
+vegaEmbed("#bar_vis", vg_2).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
